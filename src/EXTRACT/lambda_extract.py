3ef7b20896babcb9a485864f337c4288486f2fe7
joshkjman <lollipop_josh@hotmail.co.uk>
2024-11-12 16:22:00 +0000

    adding lambda_extract func

--- src/EXTRACT/lambda_extract.py
+++ src/EXTRACT/lambda_extract.py
@@ -1,6 +1,23 @@
-from src.EXTRACT.connection import connect_to_db, close_conn
-from pprint import pprint
+from pg8000.native import Connection
+from dotenv import load_dotenv
+import os
+import boto3
 import csv
+
+load_dotenv(override=True) ## states whether the existing os env variables(logins) should be overwritten by the .env file.
+
+def connect_to_db():
+    return Connection(
+        user=os.getenv("USER"),
+        password=os.getenv("PASSWORD"),
+        database=os.getenv("DATABASE"),
+        host=os.getenv("HOST"),
+        port=int(os.getenv("PORT"))
+    )
+
+def close_conn(conn):
+    conn.close()
+
 
 def read_data(table_name):
     conn = connect_to_db() # connects to the DB created in previous file 
@@ -9,7 +26,7 @@ def read_data(table_name):
     close_conn(conn)
 
     new_keys = [i for key in keys for i in key] # Flattens the list so the csv writer does not enounter any issues, otherwise it will appear as tuples 
-    with open(f'data/{table_name}.csv', 'w', newline='') as csvfile: # creates csv file in write mode 
+    with open(f'/tmp/{table_name}.csv', 'w', newline='') as csvfile: # creates csv file in write mode 
         writer = csv.writer(csvfile) # creates csv writer object 
         writer.writerow(new_keys) # writes the headers(column names) as the first row 
         writer.writerows(result) # writes all rows of data from the specified table row by row 
@@ -30,4 +47,13 @@ def read_all_tables():
     for name in table_names:
         read_data(name)
 
-read_all_tables()
+
+
+def put_data_in_s3(event, context):
+    s3 = boto3.client('s3')
+    bucket_name = "ingested-data-lambda-legends-24"
+    read_all_tables()
+    file_names = os.listdir(path='data')
+    for file_name in file_names:
+        name = file_name.split('.')[0]
+        s3.upload_file(f'/tmp/{file_name}', bucket_name, name)
