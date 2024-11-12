5eb0152845e3a5981e1da25c340f5a3d6de51d44
Coder178 <pratik.shrestha74@gmail.com>
2024-11-12 12:06:03 +0000

    new s3 lambda bucket, lambda policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,35 @@
 resource "aws_s3_bucket" "ingested_bucket"{
     bucket_prefix = "ingested-data-lambda-legends-24-"
+
+tags = {
+  Name = "ingested-data-lambda-legends-24-"
+  Eviroment = "Dev"
+  Project = "Lambda Legends Data Processor"
+
+}
+}
+
+resource "aws_s3_bucket" "lambda_code_bucket" {
+
+  Bucket_prefix = "lambda-extract-code-bucket-"
+  force_destroy = true 
+
+tags = {
+  Name = "lambda-extract-code-bucket-"
+  Eviroment = "Dev"
+  Project = "Lambda Legends Data Processor"
+
+}
+}
+
+resource "aws_s3_object" "lambda_code" {
+bucket =  aws_s3_bucket.lambda_code_bucket.bucket
+key    =  "extract_data_lambda_code.zip"
+source =  "${path.module}/../extract_data.zip"
+}
+
+resource "aws_s3_object" "layer_code" {
+bucket =  aws_s3_bucket.lambda_code_bucket.bucket
+key    =  "layer_code.zip"
+source =  "${path.module}/layer.zip"
 }
