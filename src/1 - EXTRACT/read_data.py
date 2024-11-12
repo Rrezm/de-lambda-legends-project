fc293cdb3e622a1657f7c82f2c962b6133c8f597
joshkjman <lollipop_josh@hotmail.co.uk>
2024-11-12 10:10:19 +0000

    adding folders

--- src/1 - EXTRACT/read_data.py
+++ src/1 - EXTRACT/read_data.py
@@ -2,7 +2,7 @@ from connection import connect_to_db, close_conn
 from pprint import pprint
 import csv
 
-def extract_lambda():
+def read_data():
     conn = connect_to_db() # connects to the DB created in previous file 
     result =  conn.run("SELECT * FROM sales_order;") # queries all row from 
     keys = conn.run("SELECT column_name FROM information_schema.columns WHERE table_name = 'sales_order';") # retrives column names/headers for the table
@@ -16,4 +16,4 @@ def extract_lambda():
         writer.writerows(result) # writes all rows of data from the sales_order table row by row 
 
     
-extract_lambda() # call the function 
+read_data() # call the function 
