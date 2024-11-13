659ebd1846dff05f57f3303db500fad880b93453
MirriamKM <mirriam.benson@gmail.com>
2024-11-13 11:39:32 +0000

    Update layer.tf

--- terraform/layer.tf
+++ terraform/layer.tf
@@ -8,8 +8,6 @@ resource "null_resource" "create_dependencies" {
     dependencies = filemd5("${path.module}/../requirements.txt")
   }
 }
-## Team 1 is currently working on this
-## Lambda python code, has to be zipped before uploading to aws
 
 
 
@@ -17,15 +15,15 @@ data "archive_file" "layer" {
 
   type             = "zip"
   output_file_mode = "0666"
-  source_file      = "${path.module}/dependencies"
+  source_dir      = "${path.module}/../dependencies"
   output_path      = "${path.module}/../layer/layer.zip"
+  depends_on = [null_resource.create_dependencies]
 }
 
- ## Layer is necessary as it zips ibraries, modules, or packages that your Lambda function requires to run but that aren’t included in AWS Lambda by default.
 
 resource "aws_lambda_layer_version" "lambda_layer" {
   layer_name          = "lambda_layer"
-  compatible_runtimes = "python 3.11"
-  s3_bucket           = aws_s3_bucket.ingested_bucket.bucket
-  s3_key              = "layers/layer.zip"
+
+  s3_bucket           = aws_s3_bucket.lambda_code_bucket.bucket
+  s3_key              = aws_s3_object.layer_code.key
 }
