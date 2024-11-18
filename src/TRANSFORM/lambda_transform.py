a6e1b6fa122663e33dcf6750df3e1996da038b7d
joshkjman <lollipop_josh@hotmail.co.uk>
2024-11-18 18:30:51 +0000

    adding other tables

--- src/TRANSFORM/lambda_transform.py
+++ src/TRANSFORM/lambda_transform.py
@@ -21,7 +21,33 @@ dim_staff_df = df_dict["staff_df"][["staff_id", "first_name", "last_name", "emai
 dim_staff_df["department_name"] = df_dict["department_df"][["department_name"]]
 dim_staff_df["location"] = df_dict["department_df"][["location"]]
 
-print(dim_staff_df)
+dim_counterparty_df = df_dict["counterparty_df"][["counterparty_id", "counterparty_legal_name"]]
+dim_counterparty_df["counterparty_legal_address_line_1"] = df_dict["address_df"][["address_line_1"]]
+dim_counterparty_df["counterparty_legal_address_line_2"] = df_dict["address_df"][["address_line_2"]]
+dim_counterparty_df["counterparty_legal_district"] = df_dict["address_df"][["district"]]
+dim_counterparty_df["counterparty_legal_city"] = df_dict["address_df"][["city"]]
+dim_counterparty_df["counterparty_legal_postal_code"] = df_dict["address_df"][["postal_code"]]
+dim_counterparty_df["counterparty_legal_country"] = df_dict["address_df"][["country"]]
+dim_counterparty_df["counterparty_legal_phone_number"] = df_dict["address_df"][["phone"]]
+
+dim_currency_df = df_dict["currency_df"][["currency_id", "currency_code"]] #currency name
+
+dim_design_df = df_dict["design_df"][["design_id", "design_name", "file_location", "file_name"]]
+
+dim_location_df = df_dict["address_df"][["address_line_1", "address_line_2", "district", "city", "postal_code", "country", "phone"]]
+dim_location_df["location_id"] = df_dict["sales_order_df"][["agreed_delivery_location_id"]]
+dim_location_df = dim_location_df[["location_id", "address_line_1", "address_line_2", "district", "city", "postal_code", "country", "phone"]]
+
+# dim_date_df = df_dict["sales_order_df"][[]]
+# dim_date_df = df_dict["sales_order_df"][[]]
+#print(df_dict["sales_order_df"])
+# print(dim_location_df)
+# print(dim_counterparty_df)
+
+fact_sales_df = df_dict["sales_order_df"][["sales_order_id", "counterparty_id", "units_sold", "unit_price", "currency_id", "agreed_payment_date", "agreed_delivery_date", "agreed_delivery_location_id"]] # needs create dates
+fact_sales_df["sales_staff_id"] = df_dict["sales_order_df"][["staff_id"]]
+fact_sales_df.index.name = "sales_record_id"
+print(fact_sales_df)
 
 #     table = pa.Table.from_pandas(dataframe)
 #     parquet_buffer = io.BytesIO()
