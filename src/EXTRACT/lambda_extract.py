eca3c4dc80787c50a6fa26e6cce9352f5a6696e9
joshkjman <lollipop_josh@hotmail.co.uk>
2024-11-18 11:34:05 +0000

    changing python

--- src/EXTRACT/lambda_extract.py
+++ src/EXTRACT/lambda_extract.py
@@ -60,8 +60,8 @@ def read_and_put_data(table_name, bucket_name, s3, folder_name):
     conn = connect_to_db()  # Connects to the database
     result = conn.run(f"SELECT * FROM {table_name};")  # Queries all rows
     keys = conn.run(
-        "SELECT column_name FROM information_schema.columns "
-        f"WHERE table_name = '{table_name}';"
+        """SELECT column_name FROM information_schema.columns "
+        WHERE table_name = ':tn';""", tn = table_name
     )  # Retrieves column names
     close_conn(conn)
 
