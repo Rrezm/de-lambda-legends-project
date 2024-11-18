71b490049fcb38863ac7f4cdfda0a16a28ea75dd
joshkjman <lollipop_josh@hotmail.co.uk>
2024-11-18 12:21:18 +0000

    updating again

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "db_credentials16"
+  name = "db_credentials17"
 }
 
  resource "aws_secretsmanager_secret_version" "db_credentials" {
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,18 +1,5 @@
-# variable "credentials" {
-#   description = "Database credentials"
-#   type        = map(any)
-#   sensitive   = true
-# }
-
 variable "credentials" {
-  default = {
-    cohort_id = "de_2024_09_02"
-    user = "project_team_7"
-    password = "ArveGgGcbzTaEOT"
-    host = "nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com"
-    database = "totesys"
-    port = 5432
-  }
-  sensitive = true
-  type = map(any)
+  description = "Database credentials"
+  type        = map(any)
+  sensitive   = true
 }
