030fb3bae0530ea84e13601799fae8016a365b0d
Rrezm <rrezonmripa@hotmail.co.uk>
2024-11-18 11:46:06 +0000

    Updated

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "db_credentials14"
+  name = "db_credentials15"
 }
 
  resource "aws_secretsmanager_secret_version" "db_credentials" {
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -3,4 +3,3 @@ variable "credentials" {
   type        = map(any)
   sensitive   = true
 }
-
