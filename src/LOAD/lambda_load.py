38b6144c02dbd9fdffa32c4d20f5891d9ee244b6
joshkjman <lollipop_josh@hotmail.co.uk>
2024-11-25 16:53:21 +0000

    flake8

--- src/LOAD/lambda_load.py
+++ src/LOAD/lambda_load.py
@@ -82,139 +82,139 @@ def read_data():
 
 def input_data_psql(event, context):
     conn = connect_to_db1()
-    try:
-        logger.info("Getting individual tables and loading into datawarehouse")
-        df_dict = read_data()
-        conn.run("DELETE FROM fact_sales_order;")
-        conn.run("DELETE FROM dim_staff;")
-        conn.run("DELETE FROM dim_counterparty;")
-        conn.run("DELETE FROM dim_currency;")
-        conn.run("DELETE FROM dim_design;")
-        conn.run("DELETE FROM dim_location;")
-        conn.run("DELETE FROM dim_date;")
+    # try:
+    # logger.info("Getting individual tables and loading into datawarehouse")
+    df_dict = read_data()
+    conn.run("DELETE FROM fact_sales_order;")
+    conn.run("DELETE FROM dim_staff;")
+    conn.run("DELETE FROM dim_counterparty;")
+    conn.run("DELETE FROM dim_currency;")
+    conn.run("DELETE FROM dim_design;")
+    conn.run("DELETE FROM dim_location;")
+    conn.run("DELETE FROM dim_date;")
 
-        for index, row in df_dict["staff"].iterrows():
-            conn.run("""INSERT INTO dim_staff (
-                    staff_id, first_name, last_name,
-                    department_name, location, email_address
-                    )
-                        VALUES (:si, :fn, :ln, :dn, :l, :ea);
-                    """,
-                        si=row["staff_id"],
-                        fn=row["first_name"],
-                        ln=row["last_name"],
-                        dn=row["department_name"],
-                        l=row["location"],
-                        ea=["email_address"])
+    for index, row in df_dict["staff"].iterrows():
+        conn.run("""INSERT INTO dim_staff (
+                staff_id, first_name, last_name,
+                department_name, location, email_address
+                )
+                    VALUES (:si, :fn, :ln, :dn, :l, :ea);
+                """,
+                si=row["staff_id"],
+                fn=row["first_name"],
+                ln=row["last_name"],
+                dn=row["department_name"],
+                l=row["location"],
+                ea=["email_address"])
 
-        for index, row in df_dict["counterparty"].iterrows():
-            conn.run("""INSERT INTO dim_counterparty (
-                    counterparty_id,
-                    counterparty_legal_name,
-                    counterparty_legal_address_line_1,
-                    counterparty_legal_address_line_2,
-                    counterparty_legal_district,
-                    counterparty_legal_city,
-                    counterparty_legal_postal_code,
-                    counterparty_legal_country,
-                    counterparty_legal_phone_number
-                    )
-                        VALUES (
-                    :ci, :cln, :clal1, :clal2, :cld,
-                    :clc, :clpc, :clcountry, :clpn
-                    );
-                    """,
-                    ci=row["counterparty_id"],
-                    cln=row["counterparty_legal_name"],
-                    clal1=row["counterparty_legal_address_line_1"],
-                    clal2=row["counterparty_legal_address_line_2"],
-                    cld=row["counterparty_legal_district"],
-                    clc=row["counterparty_legal_city"],
-                    clpc=row["counterparty_legal_postal_code"],
-                    clcountry=row["counterparty_legal_country"],
-                    clpn=row["counterparty_legal_phone_number"])
+    for index, row in df_dict["counterparty"].iterrows():
+        conn.run("""INSERT INTO dim_counterparty (
+                counterparty_id,
+                counterparty_legal_name,
+                counterparty_legal_address_line_1,
+                counterparty_legal_address_line_2,
+                counterparty_legal_district,
+                counterparty_legal_city,
+                counterparty_legal_postal_code,
+                counterparty_legal_country,
+                counterparty_legal_phone_number
+                )
+                    VALUES (
+                :ci, :cln, :clal1, :clal2, :cld,
+                :clc, :clpc, :clcountry, :clpn
+                );
+                """,
+                ci=row["counterparty_id"],
+                cln=row["counterparty_legal_name"],
+                clal1=row["counterparty_legal_address_line_1"],
+                clal2=row["counterparty_legal_address_line_2"],
+                cld=row["counterparty_legal_district"],
+                clc=row["counterparty_legal_city"],
+                clpc=row["counterparty_legal_postal_code"],
+                clcountry=row["counterparty_legal_country"],
+                clpn=row["counterparty_legal_phone_number"])
 
-        for index, row in df_dict["currency"].iterrows():
-            conn.run("""INSERT INTO dim_currency (
-                    currency_id, currency_code, currency_name
-                    )
-                        VALUES (:ci, :cc, :cn);
-                    """,
-                    ci=row["currency_id"],
-                    cc=row["currency_code"],
-                    cn=row["currency_name"])
+    for index, row in df_dict["currency"].iterrows():
+        conn.run("""INSERT INTO dim_currency (
+                currency_id, currency_code, currency_name
+                )
+                    VALUES (:ci, :cc, :cn);
+                """,
+                ci=row["currency_id"],
+                cc=row["currency_code"],
+                cn=row["currency_name"])
 
-        for index, row in df_dict["design"].iterrows():
-            conn.run("""INSERT INTO dim_design (
-                    design_id, design_name, file_location, file_name
-                    )
-                        VALUES (:di, :dn, :fl, :fn);
-                    """,
-                    di=row["design_id"],
-                    dn=row["design_name"],
-                    fl=row["file_location"],
-                    fn=row["file_name"])
+    for index, row in df_dict["design"].iterrows():
+        conn.run("""INSERT INTO dim_design (
+                design_id, design_name, file_location, file_name
+                )
+                    VALUES (:di, :dn, :fl, :fn);
+                """,
+                di=row["design_id"],
+                dn=row["design_name"],
+                fl=row["file_location"],
+                fn=row["file_name"])
 
-        for index, row in df_dict["location"].iterrows():
-            conn.run("""INSERT INTO dim_location (
-                    location_id, address_line_1, address_line_2,
-                    district, city, postal_code, country, phone
-                    )
-                        VALUES (:li, :al1, :al2, :d, :c, :pc, :country, :p);
-                    """,
-                    li=row["location_id"],
-                    al1=row["address_line_1"],
-                    al2=row["address_line_2"],
-                    d=row["district"],
-                    c=row["city"],
-                    pc=row["postal_code"],
-                    country=row["country"],
-                    p=["phone"])
+    for index, row in df_dict["location"].iterrows():
+        conn.run("""INSERT INTO dim_location (
+                location_id, address_line_1, address_line_2,
+                district, city, postal_code, country, phone
+                )
+                    VALUES (:li, :al1, :al2, :d, :c, :pc, :country, :p);
+                """,
+                li=row["location_id"],
+                al1=row["address_line_1"],
+                al2=row["address_line_2"],
+                d=row["district"],
+                c=row["city"],
+                pc=row["postal_code"],
+                country=row["country"],
+                p=["phone"])
 
-        for index, row in df_dict["date"].iterrows():
-            conn.run("""INSERT INTO dim_date (
-                    date_id, year, month, day,
-                    day_of_week, day_name, month_name, quarter
-                    )
-                        VALUES (:di, :y, :m, :d, :dow, :dn, :mn, :q);
-                    """,
-                    di=row["date_id"],
-                    y=row["year"],
-                    m=row["month"],
-                    d=row["day"],
-                    dow=row["day_of_week"],
-                    dn=["day_name"],
-                    mn=row["month_name"],
-                    q=row["quarter"])
+    for index, row in df_dict["date"].iterrows():
+        conn.run("""INSERT INTO dim_date (
+                date_id, year, month, day,
+                day_of_week, day_name, month_name, quarter
+                )
+                    VALUES (:di, :y, :m, :d, :dow, :dn, :mn, :q);
+                """,
+                di=row["date_id"],
+                y=row["year"],
+                m=row["month"],
+                d=row["day"],
+                dow=row["day_of_week"],
+                dn=["day_name"],
+                mn=row["month_name"],
+                q=row["quarter"])
 
-        for index, row in df_dict["fact"].iterrows():
-            conn.run("""INSERT INTO fact_sales_order
-                    (sales_order_id, created_date,
-                    created_time, last_updated_date,
-                    last_updated_time, sales_staff_id,
-                    counterparty_id, units_sold, unit_price,
-                    currency_id, design_id, agreed_payment_date,
-                    agreed_delivery_date, agreed_delivery_location_id)
-                        VALUES (
-                    :soi, :cd, :ct, :lud, :lut, :ssi,
-                    :ci, :us, :up, :currencyi, :di, :apd, :add, :adli);
-                    """,
-                    soi=row["sales_order_id"],
-                    cd=row["created_date"],
-                    ct=row["created_time"],
-                    lud=row["last_updated_date"],
-                    lut=row["last_updated_time"],
-                    ssi=row["sales_staff_id"],
-                    ci=row["counterparty_id"],
-                    us=row["units_sold"],
-                    up=row["unit_price"],
-                    currencyi=row["currency_id"],
-                    di=row["design_id"],
-                    apd=row["agreed_payment_date"],
-                    add=row["agreed_delivery_date"],
-                    adli=row["agreed_delivery_location_id"])
+    for index, row in df_dict["fact"].iterrows():
+        conn.run("""INSERT INTO fact_sales_order
+                (sales_order_id, created_date,
+                created_time, last_updated_date,
+                last_updated_time, sales_staff_id,
+                counterparty_id, units_sold, unit_price,
+                currency_id, design_id, agreed_payment_date,
+                agreed_delivery_date, agreed_delivery_location_id)
+                    VALUES (
+                :soi, :cd, :ct, :lud, :lut, :ssi,
+                :ci, :us, :up, :currencyi, :di, :apd, :add, :adli);
+                """,
+                soi=row["sales_order_id"],
+                cd=row["created_date"],
+                ct=row["created_time"],
+                lud=row["last_updated_date"],
+                lut=row["last_updated_time"],
+                ssi=row["sales_staff_id"],
+                ci=row["counterparty_id"],
+                us=row["units_sold"],
+                up=row["unit_price"],
+                currencyi=row["currency_id"],
+                di=row["design_id"],
+                apd=row["agreed_payment_date"],
+                add=row["agreed_delivery_date"],
+                adli=row["agreed_delivery_location_id"])
 
-        close_conn(conn)
-        logger.info("Successfully uploaded to the datawarehouse")
-    except Exception as e:
-        logger.error(f"Error with transformation occurred with {e}")
+    close_conn(conn)
+    #     logger.info("Successfully uploaded to the datawarehouse")
+    # except Exception as e:
+    #     logger.error(f"Error with transformation occurred with {e}")
