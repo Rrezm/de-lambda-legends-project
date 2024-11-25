8da6264930a4e2cd1e5ed58b919404108ed45fc6
joshkjman <lollipop_josh@hotmail.co.uk>
2024-11-25 16:44:14 +0000

    flake8

--- src/LOAD/lambda_load.py
+++ src/LOAD/lambda_load.py
@@ -100,12 +100,12 @@ def input_data_psql(event, context):
                     )
                         VALUES (:si, :fn, :ln, :dn, :l, :ea);
                     """,
-                    si=row["staff_id"],
-                    fn=row["first_name"],
-                    ln=row["last_name"],
-                    dn=row["department_name"],
-                    l=row["location"],
-                    ea=["email_address"])
+            si=row["staff_id"],
+            fn=row["first_name"],
+            ln=row["last_name"],
+            dn=row["department_name"],
+            l=row["location"],
+            ea=["email_address"])
 
         for index, row in df_dict["counterparty"].iterrows():
             conn.run("""INSERT INTO dim_counterparty (
@@ -215,6 +215,6 @@ def input_data_psql(event, context):
                     adli=row["agreed_delivery_location_id"])
 
         close_conn(conn)
-        logger.info(f"Successfully uploaded to the datawarehouse")
+        logger.info("Successfully uploaded to the datawarehouse")
     except Exception as e:
         logger.error(f"Error with transformation occurred with {e}")
