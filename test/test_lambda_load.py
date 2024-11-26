c88910477d886e00e46d8017d987e1b64b24bcd9
Rrezm <rrezonmripa@hotmail.co.uk>
2024-11-26 11:49:58 +0000

    Added testing for load

--- test/test_lambda_load.py
+++ test/test_lambda_load.py
@@ -1,19 +1,31 @@
 from src.LOAD.lambda_load import connect_to_db1
 from src.LOAD.lambda_load import input_data_psql
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
 
-@patch("src.LOAD.lambda_load.connect_to_db1")
-def test_connect_to_db(mock_connect_to_db):
-    mock_connect_to_db.return_value = MagicMock()
+@patch("src.LOAD.lambda_load.get_db_credentials")
+def test_connect_to_db(mock_get_db_credentials):
+    mock_get_db_credentials.return_value = {
+        "user": "test_user",
+        "password": "test_password",
+        "database": "test_database",
+        "host": "localhost",
+        "port": "5432",
+    }
     conn = connect_to_db1()
     assert conn is not None
 
 
+@patch("src.LOAD.lambda_load.get_db_credentials")
 @patch("src.LOAD.lambda_load.read_data")
-@patch("src.LOAD.lambda_load.connect_to_db1")
-def test_psql_empty(mock_connect_to_db, mock_read_data):
-    mock_connect_to_db.return_value = MagicMock()
+def test_psql_empty(mock_read_data, mock_get_db_credentials):
+    mock_get_db_credentials.return_value = {
+        "user": "test_user",
+        "password": "test_password",
+        "database": "test_database",
+        "host": "localhost",
+        "port": "5432",
+    }
     mock_read_data.return_value = {}
     result = input_data_psql({}, None)
     assert result is None
